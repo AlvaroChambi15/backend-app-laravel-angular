febb802d6ca016af815fb85012c1692d39ff6465
alvaromch <alvaromch1510@gmail.com>
2022-07-20 00:12:10 -0400

    circulo W

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -425,7 +425,12 @@
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
 
-                <h1 style="color:white; font-size:60px; margin:0px; margin-top:15px;">BA<small>KALY</small></h1>
+                <h1 style="color:white; font-size:60px; margin:0px; margin-top:15px;-webkit-text-stroke: 2px black;">
+                    BA<small>KALY</small>
+                </h1>
+                <div
+                    style="margin-top:48px;color:white;height: 40px;width: 40px;display: table-cell;text-align: center;font-size:25px;vertical-align: middle;border: 3px solid white;outline: 3px solid black;border-radius: 100%;background: #ffffff00;">
+                    W</div>
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
